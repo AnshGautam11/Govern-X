3e7a50eb4f650526ebae7f10009038eb0f9e6300
Anshii Gautam <gautamanshii509@gmail.com>
2026-08-30 00:40:23 +0530

    Organize AWS test files

--- mock_aws/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Simple mock AWS account and IAM simulation used for local testing."""
-
-from .account import MockAWSAccount, MockIAMUser, SecurityAuditPolicy
-
-__all__ = ["MockAWSAccount", "MockIAMUser", "SecurityAuditPolicy"]
