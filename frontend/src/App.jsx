576d3490779ad7995274e2b2a595b3b5351ae778
Anshii Gautam <gautamanshii509@gmail.com>
2026-08-30 00:55:49 +0530

    Update App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import React from 'react'
 import './App.css'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
+import PillarDetail from './components/PillarDetail'
 
 function App() {
-  return (
-    <div className="app">
-      <Dashboard />
-    </div>
+  return React.createElement(
+    'div',
+    { className: 'app' },
+    React.createElement(Routes, null,
+      React.createElement(Route, { path: '/', element: React.createElement(Dashboard) }),
+      React.createElement(Route, { path: '/pillar/:pillarSlug', element: React.createElement(PillarDetail) }),
+      React.createElement(Route, { path: '*', element: React.createElement(Navigate, { to: '/', replace: true }) })
+    )
   )
 }
 
